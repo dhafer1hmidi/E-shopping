c6f28dce9505d2fc6750b5e55c740490f2b10885
dhafer1hmidi <hmididhafer@yahoo.com>
2021-08-10 12:03:44 +0200

    still

---
+++ main.js
@@ -0,0 +1,67 @@
+// var men=[{
+//     image:"./image/i1.jpg",
+//     price:'240.000DT'
+// },
+// {
+//    image:"./image/i2.jpg",
+//    price:'260.000DT'
+// },
+// {
+// image:"./image/i3.jpg",
+// price:'350.000DT'
+// },
+// {
+// image:"./image/i4.jpg",
+// price:'290.000DT'
+// },
+// {
+// image:"./image/i5.jpg",
+// price:'580.000DT'
+// },
+// {
+// image:"./image/i6.jpg",
+// price:'120.000DT'
+// },
+// {
+// image:"./image/i7.jpg",
+// price:'120.000DT'
+// }
+// ]
+// var women = [ {
+//         image:"./image/x1.jpg",
+//         price:'390.000DT'
+//     },
+//     {
+//        image:"./image/x2.jpg",
+//        price:'450.000DT'
+//     },
+//     {
+//     image:"./image/x3.jpg",
+//     price:'290.000DT'
+//     },
+//     {
+//     image:"./image/x4.jpg",
+//     price:'580.000DT'
+//     },
+//     {
+//     image:"./image/x5.jpg",
+//     price:'1200.000DT'
+//     },
+//     {
+//     image:"./image/x6.jpg",
+//     price:'1100.000DT'
+//     },
+//     {
+//     image:"./image/x7.jpg",
+//     price:'1500.000DT'
+//     }
+// ]
+
+// var userName=document.getElementById("userMail").value
+//     var password=document.getElementById("pasword").value
+var test=function(){
+    var userName=document.getElementById("userMail").value
+    var password=document.getElementById("pasword").value
+    var 
+}
+localStorage.getItem('')
